d1c39bae2f3b3bf3bf255dc1ed10abd77cd23ef0
tuxzz <dorazzsoft@gmail.com>
2015-08-05 16:28:39 +0800

    auto cooked

--- net_speeder.c
+++ net_speeder.c
@@ -10,11 +10,10 @@
 /* default snap length (maximum bytes per packet to capture) */
 #define SNAP_LEN 65535
 
-#ifdef COOKED
-#define ETHERNET_H_LEN 16
-#else
-#define ETHERNET_H_LEN 14
-#endif
+#define ETHERNET_H_LEN_COOKED 16
+#define ETHERNET_H_LEN_NORMAL 14
+
+static int ethernet_h_len = -1;
 
 #define SPECIAL_TTL 88
 
@@ -26,108 +25,136 @@ void print_usage(void);
 * print help text
 */
 void print_usage(void) {
-	printf("Usage: %s [interface][\"filter rule\"]\n", "net_speeder");
-	printf("\n");
-	printf("Options:\n");
-	printf("    interface    Listen on <interface> for packets.\n");
-	printf("    filter       Rules to filter packets.\n");
-	printf("\n");
+    printf("Usage: %s [mode] interface \"filter\"\n", "net_speeder");
+    printf("\n");
+    printf("Options:\n");
+    printf("    mode         Ethernet header length(auto, normal, cooked)\n");
+    printf("                 The default is \"auto\"\n");
+    printf("    interface    Listen on <interface> for packets.\n");
+    printf("    filter       Rules to filter packets.\n");
+    printf("\n");
 }
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-	struct libnet_ipv4_hdr *ip = (struct libnet_ipv4_hdr*)(packet + ETHERNET_H_LEN);
-	if(ip->ip_ttl == SPECIAL_TTL) return;
-	switch(ip->ip_p){
-	case 6:{
-		struct libnet_tcp_hdr *tcp = (struct libnet_tcp_hdr*)(ip + 20);
-		if(tcp->th_sport == 1723 || tcp->th_dport == 1723) return;
-		break;
-	}
-	case 17:{
-		break;
-	}
-	default:
-		return;
-	}
-	libnet_t *libnet_handler = (libnet_t *)args;
-	ip->ip_ttl = SPECIAL_TTL;
-	int len_written = libnet_adv_write_raw_ipv4(libnet_handler, (u_int8_t *)ip, ntohs(ip->ip_len));
-	if(len_written < 0) {
-		printf("packet len:[%d] actual write:[%d]\n", ntohs(ip->ip_len), len_written);
-		printf("err msg:[%s]\n", libnet_geterror(libnet_handler));
-	}
+    struct libnet_ipv4_hdr *ip = (struct libnet_ipv4_hdr*)(packet + ethernet_h_len);
+    if(ip->ip_ttl == SPECIAL_TTL) return;
+    switch(ip->ip_p){
+    case 6:{
+        struct libnet_tcp_hdr *tcp = (struct libnet_tcp_hdr*)(ip + 20);
+        if(tcp->th_sport == 1723 || tcp->th_dport == 1723) return;
+        break;
+    }
+    case 17:{
+        break;
+    }
+    default:
+        return;
+    }
+    libnet_t *libnet_handler = (libnet_t *)args;
+    ip->ip_ttl = SPECIAL_TTL;
+    int len_written = libnet_adv_write_raw_ipv4(libnet_handler, (u_int8_t *)ip, ntohs(ip->ip_len));
+    if(len_written < 0) {
+        printf("packet len:[%d] actual write:[%d]\n", ntohs(ip->ip_len), len_written);
+        printf("err msg:[%s]\n", libnet_geterror(libnet_handler));
+    }
 }
 
 libnet_t* start_libnet(char *dev) {
-	char errbuf[LIBNET_ERRBUF_SIZE];
-	libnet_t *libnet_handler = libnet_init(LIBNET_RAW4_ADV, dev, errbuf);
+    char errbuf[LIBNET_ERRBUF_SIZE];
+    libnet_t *libnet_handler = libnet_init(LIBNET_RAW4_ADV, dev, errbuf);
 
-	if(NULL == libnet_handler) {
-		printf("libnet_init: error %s\n", errbuf);
-	}
-	return libnet_handler;
+    if(NULL == libnet_handler) {
+        printf("libnet_init: error %s\n", errbuf);
+    }
+    return libnet_handler;
 }
 
-#define ARGC_NUM 3
 int main(int argc, char **argv) {
-	char *dev = NULL;
-	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t *handle;
+    char *dev = NULL;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;
 
-	char *filter_rule = NULL;
-	struct bpf_program fp;
-	bpf_u_int32 net, mask;
+    char *filter_rule = NULL;
+    struct bpf_program fp;
+    bpf_u_int32 net, mask;
 
-	if (argc == ARGC_NUM) {
-		dev = argv[1];
-		filter_rule = argv[2];
-		printf("Device: %s\n", dev);
-		printf("Filter rule: %s\n", filter_rule);
-	} else {
-		print_usage();	
-		return -1;
-	}
+    if (argc == 3) {
+        
+        dev = argv[1];
+        filter_rule = argv[2];
+        printf("Device: %s\n", dev);
+        printf("Filter rule: %s\n", filter_rule);
+        
+        if(strncmp(argv[1], "venet", 5) == 0)
+            ethernet_h_len = ETHERNET_H_LEN_COOKED;
+        else
+            ethernet_h_len = ETHERNET_H_LEN_NORMAL;
+    } else if (argc == 4) {
+        if(strcmp(argv[1], "auto") == 0)
+        {
+            if(strncmp(argv[1], "venet", 5) == 0)
+                ethernet_h_len = ETHERNET_H_LEN_COOKED;
+            else
+                ethernet_h_len = ETHERNET_H_LEN_NORMAL;
+        }
+        else if(strcmp(argv[1], "normal") == 0)
+            ethernet_h_len = ETHERNET_H_LEN_NORMAL;
+        else if(strcmp(argv[1], "cooked") == 0)
+            ethernet_h_len = ETHERNET_H_LEN_COOKED;
+        else
+        {
+            print_usage();
+            return -1;
+        }
+        dev = argv[2];
+        filter_rule = argv[3];
+        printf("Device: %s\n", dev);
+        printf("Filter rule: %s\n", filter_rule);
+    } else {
+        print_usage();
+        return -1;
+    }
 
-	printf("ethernet header len:[%d](14:normal, 16:cooked)\n", ETHERNET_H_LEN);
+    printf("ethernet header len:[%d](14:normal, 16:cooked)\n", ethernet_h_len);
 
-	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-		printf("Couldn't get netmask for device %s: %s\n", dev, errbuf);
-		net = 0;
-		mask = 0;
-	}
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+        printf("Couldn't get netmask for device %s: %s\n", dev, errbuf);
+        net = 0;
+        mask = 0;
+    }
 
-	printf("init pcap\n");
-	handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
-	if(handle == NULL) {
-		printf("pcap_open_live dev:[%s] err:[%s]\n", dev, errbuf);
-		printf("init pcap failed\n");
-		return -1;
-	}
+    printf("init pcap\n");
+    handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+    if(handle == NULL) {
+        printf("pcap_open_live dev:[%s] err:[%s]\n", dev, errbuf);
+        printf("init pcap failed\n");
+        return -1;
+    }
 
-	printf("init libnet\n");
-	libnet_t *libnet_handler = start_libnet(dev);
-	if(NULL == libnet_handler) {
-		printf("init libnet failed\n");
-		return -1;
-	}
+    printf("init libnet\n");
+    libnet_t *libnet_handler = start_libnet(dev);
+    if(NULL == libnet_handler) {
+        printf("init libnet failed\n");
+        return -1;
+    }
 
-	if (pcap_compile(handle, &fp, filter_rule, 0, net) == -1) {
-		printf("filter rule err:[%s][%s]\n", filter_rule, pcap_geterr(handle));
-		return -1;
-	}
+    if (pcap_compile(handle, &fp, filter_rule, 0, net) == -1) {
+        printf("filter rule err:[%s][%s]\n", filter_rule, pcap_geterr(handle));
+        return -1;
+    }
 
-	if (pcap_setfilter(handle, &fp) == -1) {
-		printf("set filter failed:[%s][%s]\n", filter_rule, pcap_geterr(handle));
-		return -1;
-	}
+    if (pcap_setfilter(handle, &fp) == -1) {
+        printf("set filter failed:[%s][%s]\n", filter_rule, pcap_geterr(handle));
+        return -1;
+    }
 
-	while(1) {
-		pcap_loop(handle, 1, got_packet, (u_char *)libnet_handler);
-	}
+    while(1) {
+        pcap_loop(handle, 1, got_packet, (u_char *)libnet_handler);
+    }
 
-	/* cleanup */
-	pcap_freecode(&fp);
-	pcap_close(handle);
-	libnet_destroy(libnet_handler);
-	return 0;
+    /* cleanup */
+    pcap_freecode(&fp);
+    pcap_close(handle);
+    libnet_destroy(libnet_handler);
+    return 0;
 }
