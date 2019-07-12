aa3747bd23c2d35fc3adaba536191bab091abc7b
snooda <snooda@debian.debian>
2019-07-12 17:44:02 +0800

    Fix: long latency in KVM guest and chksum error in KVM guest

--- net_speeder.c
+++ net_speeder.c
@@ -45,6 +45,10 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 
 	if(ip->ip_ttl != SPECIAL_TTL) {
 		ip->ip_ttl = SPECIAL_TTL;
+		ip->ip_sum = 0;
+		struct libnet_tcp_hdr *tcp = (u_int8_t *)ip + ip->ip_hl * 4;
+		tcp->th_sum = 0;
+		libnet_do_checksum(libnet_handler, ip, IPPROTO_TCP, LIBNET_TCP_H);
 		int len_written = libnet_adv_write_raw_ipv4(libnet_handler, (u_int8_t *)ip, ntohs(ip->ip_len));
 		if(len_written < 0) {
 			printf("packet len:[%d] actual write:[%d]\n", ntohs(ip->ip_len), len_written);
@@ -95,7 +99,7 @@ int main(int argc, char **argv) {
 	}
 
 	printf("init pcap\n");
-	handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+	handle = pcap_open_live(dev, SNAP_LEN, 1, 0, errbuf);
 	if(handle == NULL) {
 		printf("pcap_open_live dev:[%s] err:[%s]\n", dev, errbuf);
 		printf("init pcap failed\n");
